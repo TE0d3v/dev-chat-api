e8d13f2b27b9bec331eab32fba2df9ec0f2580fe
Pedro <pedrohenriqueteofilo2707@gmail.com>
2025-08-19 21:36:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: { origin: '*' }
+})
+const port = 3000
+
+let users = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+io.on("connection", (socket) => {
+    console.log("um novo user conectou") 
+
+    socket.on("registerUser", (username) => {
+        users.push({ id: socket.id, username })
+
+        io.emit("userConnected", username)
+        socket.emit("systemMessage", "Bem-vindo ao chat," + username)
+    })
+
+    socket.on("sendMessage", (data) => {
+        io.emit("newMessage", { username: data.username, message: data.message })
+    })
+
+//desconectar user
+    socket.on("disconnect", () => {
+        console.log("user desconectou")
+    })
+});
+
+
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
